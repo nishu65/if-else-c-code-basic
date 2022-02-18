f8fc136d6fe7585d2116186bd438c193e740bb90
nishu65 <nishubhaiqwerty@gmail.com>
2022-02-18 09:59:53 +0530

    Initial commit

---
+++ nH2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout << "enter your age";
+	cin >>a;
+	if (a > 18) {
+		cout << "you are adult.";
+	}
+	else{
+		cout << "you are child.GO FUCK OF MAOTHERFUCKER";
+	}
+	return 0;
+}
